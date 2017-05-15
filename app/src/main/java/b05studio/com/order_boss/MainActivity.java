60d6e5ea4440e5dd6111091e9e015083d7215b4d
unknown <akstn3023@naver.com>
2017-05-16 03:29:13 +0900

    add toolbar to mainActivity

--- app/src/main/java/b05studio/com/order_boss/MainActivity.java
+++ app/src/main/java/b05studio/com/order_boss/MainActivity.java
@@ -2,6 +2,9 @@ package b05studio.com.order_boss;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
     }
 }
