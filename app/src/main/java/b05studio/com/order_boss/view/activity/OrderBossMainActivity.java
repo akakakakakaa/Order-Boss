97f9875522abe972ea3a644a5789d547dbd3c79b
young <siosio34@nate.com>
2017-05-16 02:48:08 +0900

    make icon

---
+++ app/src/main/java/b05studio/com/order_boss/view/activity/OrderBossMainActivity.java
@@ -0,0 +1,16 @@
+package b05studio.com.order_boss.view.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import b05studio.com.order_boss.R;
+
+public class OrderBossMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order_boss_main);
+
+    }
+}
