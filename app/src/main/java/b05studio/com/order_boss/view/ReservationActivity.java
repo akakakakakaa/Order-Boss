534982193a9065543cd00ec5c39b2b8eadb37d1c
unknown <akstn3023@naver.com>
2017-05-28 18:18:38 +0900

    add restaurantActivity menu, info, review

---
+++ app/src/main/java/b05studio/com/order_boss/view/ReservationActivity.java
@@ -0,0 +1,10 @@
+package b05studio.com.order_boss.view;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by mansu on 2017-05-28.
+ */
+
+public class ReservationActivity extends AppCompatActivity {
+}
