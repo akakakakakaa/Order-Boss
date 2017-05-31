14b0d5535c46216b7b378617d998473d866917ab
young <siosio34@nate.com>
2017-06-01 03:10:49 +0900

    add login activity

---
+++ app/src/main/java/b05studio/com/order_boss/view/activity/LoginActivity.java
@@ -0,0 +1,20 @@
+package b05studio.com.order_boss.view.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import b05studio.com.order_boss.R;
+
+/**
+ * Created by young on 2017-05-31.
+ */
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+}
