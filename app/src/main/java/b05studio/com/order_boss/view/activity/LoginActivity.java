e9b39b380b1c36196687b6e188bede9924c97984
young <siosio34@nate.com>
2017-06-02 05:23:10 +0900

    make vibrate method

--- app/src/main/java/b05studio/com/order_boss/view/activity/LoginActivity.java
+++ app/src/main/java/b05studio/com/order_boss/view/activity/LoginActivity.java
@@ -1,8 +1,15 @@
 package b05studio.com.order_boss.view.activity;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Vibrator;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.NotificationCompat;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -33,6 +40,7 @@ public class LoginActivity extends AppCompatActivity {
                 login(loginEditIdText.getText().toString(),loginEditPassText.getText().toString());
             }
         });
+        sendNotification();
     }
 
     private void login(String id, String pass) {
@@ -44,6 +52,30 @@ public class LoginActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
+    private void sendNotification() {
+            Intent intent = new Intent(this, MainActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
+                    PendingIntent.FLAG_ONE_SHOT);
 
+            Uri defaultSoundUri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+            NotificationCompat.Builder notificationBuilder = (NotificationCompat.Builder) new NotificationCompat.Builder(this)
+                    .setSmallIcon(R.drawable.icon_jungyeon)
+                    .setContentTitle("순번대장")
+                    .setContentText("고객님의 주문하신 \"브리또\"가 앞으로 5분뒤면 완성됩니다!")
+                    .setAutoCancel(true)
+                    .setSound(defaultSoundUri)
+                    .setContentIntent(pendingIntent);
 
+            NotificationManager notificationManager =
+                    (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+            notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
+            Vibrator vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+            vibrator.vibrate(2000);
+        }
 }
+
+
+
+
